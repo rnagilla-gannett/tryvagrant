35d3734aefd207cac79b3264d510b3142732c8e4
rnagilla <rnagilla.gannett@gmail.com>
2013-09-19 15:46:43 -0400

    Vagrant Working

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spree::Application.config.secret_key_base = 'f5840610bfce6b6e3ad1b28144f3d520b9ba31b9f22fbe139fe758cff384e2620241163ab534c44da52b9d849572b5cb703ec07e8b6736228245bd62f85f0826'
